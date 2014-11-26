f92c6f4d1f19db9c6ec337ca44a8749b7ba60c77
Bastian de Groot <git@de-groot.info>
2014-11-26 18:10:38 +0100

    * conkyrc for cpu usage and ram * tint2 as a panel * openbox config and autostart

---
+++ openbox/autostart.sh
@@ -0,0 +1,22 @@
+xset +dpms &
+~/scripts/monitor.sh &
+~/scripts/poll.sh &
+tint2 &
+conky &
+xterm -e "~/scripts/tc.sh" &
+yakuake &
+~/scripts/thunderbird.sh &
+easystroke &
+keepass2 &
+gnome-do &
+xscreensaver -no-splash &
+setxkbmap de nodeadkeys &
+setxkbmap -option ctrl:nocaps &
+owncloud &
+kmix &
+dunst &
+gvim ~/ownCloud/TODO &
+gtk-redshift -l 50.11:8.68 &
+nm-applet &
+kill -HUP $(pidof conky) & # TODO dirty hack
+xset b off
