d0dd91fa896bdaaabf22d7f43ffbb65210b7f428
Bastian de Groot <git@de-groot.info>
2018-05-23 11:50:02 +0200

    add pe command for running a shell in a certain pod

--- bash/functions/14_kubectl.sh
+++ bash/functions/14_kubectl.sh
@@ -19,3 +19,14 @@ pd() {
     pod_ns="$(echo "$pod_line" | awk '{print $1}')"
     kubectl describe pod "$pod_id" --namespace "$pod_ns"
 }
+
+pe() {
+    if [ -z "$1" ] ; then
+        echo "pattern missing" >&2
+        return 1
+    fi
+    pod_line="$(kubectl get pods --all-namespaces | grep -i "$1" -m 1)"
+    pod_id="$(echo "$pod_line" | awk '{print $2}')"
+    pod_ns="$(echo "$pod_line" | awk '{print $1}')"
+    kubectl exec -it "$pod_id" --namespace "$pod_ns" -- /bin/bash
+}
