cc9e0772e0e43ec830b670e3034d14817aafcea7
Bastian de Groot <git@de-groot.info>
2021-03-31 15:01:13 +0200

    Install language servers in dotfile update

--- bash/functions/99_update_dotfiles.sh
+++ bash/functions/99_update_dotfiles.sh
@@ -1,6 +1,6 @@
 update_dotfiles() {
-	if [ "$(which git)" ]; then
-		if [ ! -e ~/dotfiles/ ] ; then
+	if command -v git &> /dev/null ; then
+		if [[ ! -e ~/dotfiles/ ]] ; then
 			# use the ssh variant if it's me
 			if [ -e "$HOME/.ssh/id_rsa" -a \( "$USER" = "bastian" -o "$USER" = "bg" -o "$USER" = "bdegroot" \) ] ; then
 				git clone --recursive git@github.com:bastiandg/dotfiles.git "$HOME/dotfiles"
@@ -14,13 +14,17 @@ update_dotfiles() {
 		cp -r "$HOME/dotfiles/bashrc" "$HOME/.bashrc"
 		rm -rf "$HOME/.bash"
 		cp -r "$HOME/dotfiles/bash" "$HOME/.bash"
-		if [ -n "$(which nvim)" ] ; then
+		if command -v nvim &> /dev/null ; then
 			rm -rf "$HOME/.vim/" "$HOME/.vimrc" "$HOME/.config/nvim"
 			ln -s "$HOME/.vim/init.vim" "$HOME/.vimrc"
 			cp -r "$HOME/dotfiles/vim" "$HOME/.vim"
 			ln -s "$HOME/.vim" "$HOME/.config/nvim"
 			nvim +PlugInstall +qall
 			pip3 install --upgrade neovim
+			pip3 install --upgrade python-language-server black pyls-black
+			if command -v go &> /dev/null ; then
+				GO111MODULE=on go get golang.org/x/tools/gopls@latest
+			fi
 			nvim +UpdateRemotePlugins +qall
 		fi
 	else
