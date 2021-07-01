40e250a4b1c42448007f833582dcfeb094dd6b44
Bastian de Groot <git@de-groot.info>
2021-07-01 10:29:33 +0200

    Generate completions on setup not in runtime

--- bash/functions/14_kubectl.sh
+++ bash/functions/14_kubectl.sh
@@ -2,91 +2,87 @@ alias k="kubectl"
 alias kg="kubectl get"
 alias kd="kubectl describe"
 
-if command -v kubectl &>/dev/null; then
-	source <(kubectl completion bash)
-fi
-
 complete -F _complete_alias k
 complete -F _complete_alias kg
 complete -F _complete_alias kd
 
 pl() {
-	if [ -z "$1" ]; then
-		kubectl get pods --all-namespaces
-	else
-		kubectl get pods --all-namespaces | grep -i "$1"
-	fi
+  if [ -z "$1" ]; then
+    kubectl get pods --all-namespaces
+  else
+    kubectl get pods --all-namespaces | grep -i "$1"
+  fi
 }
 
 pd() {
-	if [ -z "$1" ]; then
-		echo "pattern missing" >&2
-		return 1
-	fi
-	pod_line="$(kubectl get pods --all-namespaces | grep -i "$1" -m 1)"
-	pod_id="$(echo "$pod_line" | awk '{print $2}')"
-	pod_ns="$(echo "$pod_line" | awk '{print $1}')"
-	if [ -n "$(command -v jq 2>/dev/null)" ]; then
-		kubectl get pod "$pod_id" --namespace "$pod_ns" -o json | jq .
-	else
-		kubectl describe pod "$pod_id" --namespace "$pod_ns"
-	fi
-	echo -e '\n\033[01;34mLogs:\033[00m'
-	kubectl logs --tail=20 -n "$pod_ns" "$pod_id"
+  if [ -z "$1" ]; then
+    echo "pattern missing" >&2
+    return 1
+  fi
+  pod_line="$(kubectl get pods --all-namespaces | grep -i "$1" -m 1)"
+  pod_id="$(echo "$pod_line" | awk '{print $2}')"
+  pod_ns="$(echo "$pod_line" | awk '{print $1}')"
+  if [ -n "$(command -v jq 2>/dev/null)" ]; then
+    kubectl get pod "$pod_id" --namespace "$pod_ns" -o json | jq .
+  else
+    kubectl describe pod "$pod_id" --namespace "$pod_ns"
+  fi
+  echo -e '\n\033[01;34mLogs:\033[00m'
+  kubectl logs --tail=20 -n "$pod_ns" "$pod_id"
 }
 
 ph() {
-	if [ -z "$1" ]; then
-		echo "pattern missing" >&2
-		return 1
-	fi
-	container_id="${2:-}"
-	tail="${3:-200}"
-	pod_line="$(kubectl get pods --all-namespaces | grep -i "$1" -m 1)"
-	pod_id="$(echo "$pod_line" | awk '{print $2}')"
-	pod_ns="$(echo "$pod_line" | awk '{print $1}')"
-	if [ -z "$container_id" ]; then
-		kubectl logs --tail="$tail" -n "$pod_ns" "$pod_id"
-	else
-		kubectl logs --tail="$tail" -c "$container_id" -n "$pod_ns" "$pod_id"
-	fi
+  if [ -z "$1" ]; then
+    echo "pattern missing" >&2
+    return 1
+  fi
+  container_id="${2:-}"
+  tail="${3:-200}"
+  pod_line="$(kubectl get pods --all-namespaces | grep -i "$1" -m 1)"
+  pod_id="$(echo "$pod_line" | awk '{print $2}')"
+  pod_ns="$(echo "$pod_line" | awk '{print $1}')"
+  if [ -z "$container_id" ]; then
+    kubectl logs --tail="$tail" -n "$pod_ns" "$pod_id"
+  else
+    kubectl logs --tail="$tail" -c "$container_id" -n "$pod_ns" "$pod_id"
+  fi
 }
 
 px() {
-	if [ -z "$1" ]; then
-		echo "pattern missing" >&2
-		return 1
-	fi
-	container_id="${2:-}"
-	pod_line="$(kubectl get pods --all-namespaces | grep -i "$1" -m 1)"
-	pod_id="$(echo "$pod_line" | awk '{print $2}')"
-	pod_ns="$(echo "$pod_line" | awk '{print $1}')"
-	if [ -z "$container_id" ]; then
-		kubectl exec -it "$pod_id" --namespace "$pod_ns" -- /bin/sh
-	else
-		kubectl exec -it "$pod_id" -c "$container_id" --namespace "$pod_ns" -- /bin/sh
-	fi
+  if [ -z "$1" ]; then
+    echo "pattern missing" >&2
+    return 1
+  fi
+  container_id="${2:-}"
+  pod_line="$(kubectl get pods --all-namespaces | grep -i "$1" -m 1)"
+  pod_id="$(echo "$pod_line" | awk '{print $2}')"
+  pod_ns="$(echo "$pod_line" | awk '{print $1}')"
+  if [ -z "$container_id" ]; then
+    kubectl exec -it "$pod_id" --namespace "$pod_ns" -- /bin/sh
+  else
+    kubectl exec -it "$pod_id" -c "$container_id" --namespace "$pod_ns" -- /bin/sh
+  fi
 }
 
 kc() {
-	if [ -z "$1" ]; then
-		echo "pattern missing" >&2
-		return 1
-	fi
-	context="$(kubectl config view -o json |
-		jq -r ".contexts[] |
+  if [ -z "$1" ]; then
+    echo "pattern missing" >&2
+    return 1
+  fi
+  context="$(kubectl config view -o json |
+    jq -r ".contexts[] |
 		select(
 			(.name | test(\"$1\"; \"i\")) or
 			(.context.cluster | test(\"$1\"; \"i\"))
 		) | .name")"
-	context_count="$(wc -l <<<"$context")"
-	if [[ "$context_count" == 0 ]]; then
-		echo "Pattern '$1' not found in kubectl contexts" >&2
-		return 2
-	elif [[ "$context_count" -gt 1 ]]; then
-		echo -e "Pattern '$1' ambiguous. Results: \n$context" >&2
-		return 3
-	else
-		kubectl config use-context "$context"
-	fi
+  context_count="$(wc -l <<<"$context")"
+  if [[ "$context_count" == 0 ]]; then
+    echo "Pattern '$1' not found in kubectl contexts" >&2
+    return 2
+  elif [[ "$context_count" -gt 1 ]]; then
+    echo -e "Pattern '$1' ambiguous. Results: \n$context" >&2
+    return 3
+  else
+    kubectl config use-context "$context"
+  fi
 }
--- bash/functions/99_update_dotfiles.sh
+++ bash/functions/99_update_dotfiles.sh
@@ -1,34 +1,40 @@
 update_dotfiles() {
-	if command -v git &> /dev/null ; then
-		if [[ ! -e ~/dotfiles/ ]] ; then
-			# use the ssh variant if it's me
-			if [ -e "$HOME/.ssh/id_rsa" -a \( "$USER" = "bastian" -o "$USER" = "bg" -o "$USER" = "bdegroot" \) ] ; then
-				git clone --recursive git@github.com:bastiandg/dotfiles.git "$HOME/dotfiles"
-			else
-				git clone --recursive https://github.com/bastiandg/dotfiles.git "$HOME/dotfiles"
-			fi
-		else
-			(cd "$HOME/dotfiles/" && git pull && git submodule update --init --recursive)
-		fi
-		cp -r "$HOME/dotfiles/bash_profile" "$HOME/.bash_profile"
-		cp -r "$HOME/dotfiles/bashrc" "$HOME/.bashrc"
-		rm -rf "$HOME/.bash"
-		cp -r "$HOME/dotfiles/bash" "$HOME/.bash"
-		if command -v nvim &> /dev/null ; then
-			rm -rf "$HOME/.vim/" "$HOME/.vimrc" "$HOME/.config/nvim"
-			ln -s "$HOME/.vim/init.vim" "$HOME/.vimrc"
-			cp -r "$HOME/dotfiles/vim" "$HOME/.vim"
-			ln -s "$HOME/.vim" "$HOME/.config/nvim"
-			nvim +PlugInstall +qall
-			pip3 install --upgrade neovim
-			pip3 install --upgrade python-language-server black pyls-black
-			if command -v go &> /dev/null ; then
-				GO111MODULE=on go get golang.org/x/tools/gopls@latest
-			fi
-			nvim +UpdateRemotePlugins +qall
-		fi
-	else
-		echo "git is not installed"
-	fi
-	source "$HOME/.bashrc"
+  if command -v git &>/dev/null; then
+    if [[ ! -e ~/dotfiles/ ]]; then
+      # use the ssh variant if it's me
+      if [ -e "$HOME/.ssh/id_rsa" -a \( "$USER" = "bastian" -o "$USER" = "bg" -o "$USER" = "bdegroot" \) ]; then
+        git clone --recursive git@github.com:bastiandg/dotfiles.git "$HOME/dotfiles"
+      else
+        git clone --recursive https://github.com/bastiandg/dotfiles.git "$HOME/dotfiles"
+      fi
+    else
+      (cd "$HOME/dotfiles/" && git pull && git submodule update --init --recursive)
+    fi
+    cp -r "$HOME/dotfiles/bash_profile" "$HOME/.bash_profile"
+    cp -r "$HOME/dotfiles/bashrc" "$HOME/.bashrc"
+    rm -rf "$HOME/.bash"
+    cp -r "$HOME/dotfiles/bash" "$HOME/.bash"
+    if command -v helm &>/dev/null; then
+      helm completion bash >"$HOME/.bash/completions/helm.sh"
+    fi
+    if command -v kubectl &>/dev/null; then
+      kubectl completion bash >"$HOME/.bash/completions/kubectl.sh"
+    fi
+    if command -v nvim &>/dev/null; then
+      rm -rf "$HOME/.vim/" "$HOME/.vimrc" "$HOME/.config/nvim"
+      ln -s "$HOME/.vim/init.vim" "$HOME/.vimrc"
+      cp -r "$HOME/dotfiles/vim" "$HOME/.vim"
+      ln -s "$HOME/.vim" "$HOME/.config/nvim"
+      nvim +PlugInstall +qall
+      pip3 install --upgrade neovim
+      pip3 install --upgrade python-language-server black pyls-black
+      if command -v go &>/dev/null; then
+        GO111MODULE=on go get golang.org/x/tools/gopls@latest
+      fi
+      nvim +UpdateRemotePlugins +qall
+    fi
+  else
+    echo "git is not installed"
+  fi
+  source "$HOME/.bashrc"
 }
