c1b80b200b2b2a875c92c9bcbdbe27f94d3ed82f
Bastian de Groot <git@de-groot.info>
2024-06-08 19:35:10 +0200

    nvim: remove trailing spaces on save

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -42,3 +42,13 @@ opt.cursorline = true
 vim.o.scrolloff = 999       -- center cursor
 
 vim.cmd("colorscheme base16-hardcore")
+
+-- Remove trailing spaces on save
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = {"*"},
+    callback = function()
+      local save_cursor = vim.fn.getpos(".")
+      pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+      vim.fn.setpos(".", save_cursor)
+    end,
+})
